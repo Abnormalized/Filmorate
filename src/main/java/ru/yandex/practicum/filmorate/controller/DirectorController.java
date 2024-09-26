72891b7aa58dd017cfc9d98f25a7a17d6dc8893f
abnormalize <Zhora.a.a@yandex.ru>
2024-09-26 16:46:44 +0300

    Filmorate #7 codestylefix

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
