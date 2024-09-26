6897a596d7561e66d6ec258b9bf2d9948681780e
abnormalize <Zhora.a.a@yandex.ru>
2024-09-26 17:27:37 +0300

    Filmorate #7 fix: Изменения согласно codereview №3

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Director;
@@ -9,7 +8,7 @@ import ru.yandex.practicum.filmorate.service.DirectorService;
 
 import java.util.Collection;
 
-@Slf4j
+
 @RestController
 @RequestMapping("/directors")
 @RequiredArgsConstructor
