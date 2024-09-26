ff762a26bc92ef8569d59e54c7877be48034789d
iivantsov <iiv.88@yandex.ru>
2024-09-26 20:26:52 +0300

    Filmorate #10 Добавлена модель Отзыва и соответствующая ей таблица в БД

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class Review {
+    private long id;
+    @NotNull
+    private Long userId;
+    @NotNull
+    private Long filmId;
+    @Min(value = 0L)
+    private long usefulRating;
+    @NotNull
+    private Boolean isPositive;
+    @NotBlank
+    private String content;
+}
