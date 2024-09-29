c8c9739161d07655828399b26b31b378e41c189f
Aleksandr Ivanov <sasha942710@yandex.ru>
2024-09-29 18:40:16 +0300

    Filmorate #6 исправил замечания.

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/UserDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.impl;
 
 import lombok.AllArgsConstructor;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
