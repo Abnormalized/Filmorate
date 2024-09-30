ae7c943b0a4f06525e6efe9024610ddb463f68d9
iivantsov <iiv.88@yandex.ru>
2024-09-30 13:50:48 +0300

    Filmorate #9 Фикс для прохождения коллекции тестов add-search

--- src/main/java/ru/yandex/practicum/filmorate/storage/mapper/RatingMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mapper/RatingMapper.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.storage.mapper;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Rating;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class RatingMapper implements RowMapper<Rating> {
     @Override
     public Rating mapRow(ResultSet rs, int rowNum) throws SQLException {
